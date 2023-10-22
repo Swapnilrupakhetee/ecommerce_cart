ce8f1851e43e9255cff3df79884bf7f0e7d0a6ea
Swapnil <rupakheteeswapnil@gmail.com>
2023-10-22 23:46:17 +0545

    9th commit

--- src/context/Shop_context.jsx
+++ src/context/Shop_context.jsx
@@ -1,7 +1,18 @@
 import React, { createContext } from 'react'
+import { PRODUCTS } from '../Products'
 export const ShopContext=createContext(null)
 
+const getDefaultCart = () =>{
+  let cart={}
+  for (let i=0; i < PRODUCTS.length;i++)
+  {
+    cart[i]=0;
+  }
+  return cart
+}
+
 function Shop_context(props) {
+  const[cartItems,setCartItems] = useState();
   return (
     <ShopContext.Provider>
       {props.children}
