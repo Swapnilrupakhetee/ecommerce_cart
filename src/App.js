2a02b373c3f3a17839d0c0e5be05ee8e563d3aa3
Swapnil <rupakheteeswapnil@gmail.com>
2023-10-23 00:16:54 +0545

    11th commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Cart from './pages/cart/Cart';
 import Shop from './pages/shop/Shop';
+import { ShopContextProvider } from './context/Shop_context';
 function App() {
   return (
     <div className="App">
+      <ShopContextProvider>
       <Router>
         <Navbar/>
 
@@ -16,6 +18,7 @@ function App() {
 
         </Routes>
       </Router>
+      </ShopContextProvider>
     </div>
   );
 }
--- src/pages/shop/Product.jsx
+++ src/pages/shop/Product.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../../context/Shop_context';
 
 function Product(props) {
   const id = props.data.id;
   const productName = props.data.productName;
   const price = props.data.price;
   const productImage = props.data.productImage;
+
+  const {addToCart,cartItems}=useContext(ShopContext);
+  const cartItemAmount=cartItems[id];
 
   return (
     <div className='product'>
@@ -13,7 +17,8 @@ function Product(props) {
         <b><p>{productName}</p></b>
         <p>$ {price}</p>
         </div>
-        <button className='addToCartBttn'>Add to Cart</button>
+        <button className='addToCartBttn' onClick={()=>addToCart(id)}>Add to Cart {cartItemAmount>0 && <>({cartItemAmount})</>}</button>
+        
     </div>
   )
 }
